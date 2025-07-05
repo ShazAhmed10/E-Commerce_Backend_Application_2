bf4b1bc53343353a3ce3a5ca903bb509c9f54357
ShazAhmed10 <5191sak@gmail.com>
2025-07-05 11:59:07 +0530

    first commit

---
+++ src/test/java/org/ecommerce_backend/ecommerce_backend_application_2/ECommerceBackendApplication2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ecommerce_backend.ecommerce_backend_application_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBackendApplication2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
